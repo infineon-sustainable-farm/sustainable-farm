fc1801e3a2b3f4333b414792530e6ba7c41c029c
ALIX6S <carinevebamba3@gmail.com>
2026-09-20 21:53:44 +0000

    Revert "feat(vm-frontend): add visitor management module shell and dashboard screen"
    This reverts commit 99e1f09139d1416bcf75f3a89b8eaf6dcc5c8ea7.

--- frontend/src/features/visitormanagement/routes.jsx
+++ frontend/src/features/visitormanagement/routes.jsx
@@ -1,12 +1,1 @@
-import VisitorManagementLayout from "./components/VisitorManagementLayout";
-import DashboardPage from "./components/DashboardPage";
-
-export default [
-    {
-        path: "visitormanagement",
-        element: <VisitorManagementLayout />,
-        children: [
-            { index: true, element: <DashboardPage /> },
-        ],
-    },
-];
+export default [];
--- frontend/src/shared/utils/formatEnumLabel.js
+++
@@ -1,6 +0,0 @@
-export function formatEnumLabel(value) {
-  return value
-    .split("_")
-    .map((word) => word[0] + word.slice(1).toLowerCase())
-    .join(" ");
-}
